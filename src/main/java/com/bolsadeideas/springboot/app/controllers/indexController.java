c96508a91184de19246a35b4f21a2db857924f2d
jiz10 <joseezambrana@gmail.com>
2019-01-16 18:08:16 -0300

    Add bootstrap

--- src/main/java/com/bolsadeideas/springboot/app/controllers/indexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/indexController.java
@@ -1,14 +1,18 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
+	@Value("${application.controller.mensaje}")
+	private String mensaje;
+	
 	@GetMapping("/")
 	public String hola(Model model) {
-		model.addAttribute("mensaje","Hola que tal Spring boot");
+		model.addAttribute("mensaje",mensaje);
 		return "hola";
 	}
 }
